e2aaa96707475182002a445317fa1970d27d9c2c
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-11-29 23:57:21 +0700

    initial project'

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10; // Variable declaration
+        int num2 = 20;
+        int sum = num1 + num2; // Expression
+        System.out.println("Sum: " + sum); // Output
+    }
+}
